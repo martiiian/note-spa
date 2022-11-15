a880b7f9459c76333f76ec98a8debc1dfc2ccd3d
martiiian <jkramorevx@gmail.com>
2022-11-15 12:51:36 +0300

    Init backend service

---
+++ app/backend/server.ts
@@ -0,0 +1,30 @@
+import * as http from 'http'
+
+import express = require('express')
+import cors = require('cors')
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
+const app = express()
+
+app.use(express.static('dist'))
+const server = http.createServer(app)
+
+const options = {
+  origin: process.env.FRONT_URL
+}
+
+app.use(cors(options))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept'
+  )
+  next()
+})
+
+server.listen(process.env.PORT, () => {
+  console.log('Server started on', process.env.PORT)
+})
